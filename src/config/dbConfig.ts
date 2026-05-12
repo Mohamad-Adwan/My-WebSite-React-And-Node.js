a513726902acbfc7557eb7614fe6b45909b6f790
Mohammad Adwan <124261417+Mohamad-Adwan@users.noreply.github.com>
2026-05-12 21:40:12 +0300

    Change API URL in dbConfig
    Updated the API URL in the database configuration.

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -2,5 +2,5 @@
 // Replace these values with your actual database connection details
 export const dbConfig = {
   // apiUrl: 'http://localhost:3000/api', // API URL for your local Node.js server
-   apiUrl: 'https://my-website-react-and-node-js-z1jd.onrender.com/api',
+   apiUrl: 'https://my-website-react-and-node-js-121.onrender.com',
 };
