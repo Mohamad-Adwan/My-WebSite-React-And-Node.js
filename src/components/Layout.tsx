997b8e9a5f90cc47ae127901b3e511db7246ce15
Mohammad Adwan <124261417+Mohamad-Adwan@users.noreply.github.com>
2026-05-27 14:54:50 +0300

    Add LiveChat component to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-
+import LiveChat from './LiveChat';
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -14,6 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <main className="flex-grow w-full max-w-screen-2xl mx-auto">
         {children}
       </main>
+      <LiveChat />
       <Footer />
     </div>
   );
